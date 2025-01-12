391731efeb19235a30cb9bed210bca03af109c54
ZEZE1020 <ogembogodfrey75@gmail.com>
2025-01-13 00:11:33 +0300

    Database refactor

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -109,7 +109,17 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 # settings.py
 
-DATABASES = { 'default': dj_database_url.config(conn_max_age=600) }
+
+DATABASES = { 
+    'default': { 
+        'ENGINE': 'django.db.backends.postgresql', 
+        'NAME': config('DATABASE_NAME'), 
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'), 
+        'HOST': config('DATABASE_HOST'), 
+        'PORT': config('DATABASE_PORT'),
+     } 
+}
 
 
 
