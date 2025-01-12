7989721107b4706f9ed4e5eba5f0ee39bc93a05a
ZEZE1020 <ogembogodfrey75@gmail.com>
2025-01-13 01:55:53 +0300

    athentication method reconfiger

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -76,9 +76,19 @@ SITE_ID = 1
  # URL configuration 
 LOGIN_REDIRECT_URL = '/' 
 LOGOUT_REDIRECT_URL = '/' 
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username'
 # Email Verification 
 ACCOUNT_EMAIL_VERIFICATION = 'none' 
-ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_REQUIRED = False
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'your_email_host'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your_email_user'
+EMAIL_HOST_PASSWORD = 'your_email_password'
+
 
 # Allauth settings
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email' 
