ee04ad28ebd1cd2e3413ed4a39fef4beb550c2d4
ZEZE1020 <ogembogodfrey75@gmail.com>
2025-01-13 04:57:17 +0300

    Update host, bug dix

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['social-media-api-a5m3.onrender.com']
+ALLOWED_HOSTS = ['social-media-api-a5m3.onrender.com', '127.0.0.1:8000']
 
 
 # Application definition
